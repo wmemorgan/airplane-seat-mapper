72e4dc2374b7b9a61d80965ed1dc3ca14248eb25
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-08-31 11:52:05 +0800

    Add data validation tests

--- src/test.py
+++ src/test.py
@@ -9,6 +9,7 @@ class SeatDataTests(unittest.TestCase):
                                    './/{http://www.opentravel.org/OTA/2003/05/common/}SeatInfo')
         self.rows = read_xml_file("OTA_AirSeatMapRS.xml",
                                   './/{http://www.opentravel.org/OTA/2003/05/common/}RowInfo')
+        self.seat_data = get_seat_data()
 
     def test_read_xml_file(self):
         seats = read_xml_file("OTA_AirSeatMapRS.xml",
@@ -27,6 +28,19 @@ class SeatDataTests(unittest.TestCase):
         create_json_file(test_data, "test-data.json")
         assert os.path.exists("../output/test-data.json")
         os.remove("../output/test-data.json")
+
+    def test_cabin_class(self):
+        for seat in self.seat_data:
+            if int(seat["seatnumber"][:-1]) > 6:
+                self.assertTrue(seat["class"] == "Economy")
+            else:
+                self.assertTrue(seat["class"] == "First")
+    
+    def test_price_data(self):
+        for seat in self.seat_data:
+            if seat["isavailable"]:
+                self.assertTrue(seat["price"] > 0)
+
 
 
 if __name__ == '__main__':
